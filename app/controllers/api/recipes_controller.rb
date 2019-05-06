f89d00a38b01725c01128e585334589a0b26d1c0
robocop019 <chrisbowen@comcast.net>
2019-05-06 12:14:00 -0500

    added restful routes

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,44 @@
 class Api::RecipesController < ApplicationController
-  def one_recipe_action
-    @recipe = Recipe.find(2)
-    render 'one_recipe_view.json.jbuilder'
+  def index
+    @recipes = Recipe.all
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        prep_time: params[:prep_time],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions],
+                        image_url: params[:image_url]
+                      )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end 
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render json: {message: "Successfully destroyed recipe."}
   end
 end
